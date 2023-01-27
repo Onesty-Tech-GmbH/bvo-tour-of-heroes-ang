dc22eb2888148ee06831d5642d4c338752acd8c0
Markus Schulz <msc@onesty-tech.de>
2023-01-27 09:30:59 +0100

    Schritt 1: manuelles Bootstrap via "Mount" Funktion im window

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import * as H from "history";
+import {AppModule} from './app/app.module';
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+(window as any)["mount"] = (containerId: string, tokens?: any, history?: H.History) => {
+	//create the element for "selector" of the AppModule
+	let targetElement = document.getElementById(containerId);
+	let appRootElement = document.createElement("app-root");
+	targetElement!.append(appRootElement);
+	platformBrowserDynamic([])
+		.bootstrapModule(AppModule, {})
+		.catch(err => console.error(err));
+}
